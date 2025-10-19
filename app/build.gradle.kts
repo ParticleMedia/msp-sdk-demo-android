e632c97ca8bb3b1331d01d861a3cc2b1d273505a
yangling1985 <85200577+yangling1985@users.noreply.github.com>
2025-10-19 09:59:10 -0700

    upgrade to v2.9.0 (#26)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
     }
 }
 
-val mspSdkVersion = "2.8.0"
+val mspSdkVersion = "2.9.0"
 
 dependencies {
     // MSP SDK dependencies:  =================== START ===============================
