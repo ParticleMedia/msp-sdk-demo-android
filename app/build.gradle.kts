6eb6e61e5cb8ce50aa73203ffb43e33a60f36020
han.xiang <han.xiang@newsbreak.com>
2025-11-26 14:51:01 -0800

    upgrade to 2.13.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
     }
 }
 
-val mspSdkVersion = "2.12.1"
+val mspSdkVersion = "2.13.0"
 
 dependencies {
     // MSP SDK dependencies:  =================== START ===============================
